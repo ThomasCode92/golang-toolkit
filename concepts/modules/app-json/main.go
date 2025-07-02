d33c595cb2cd73d64618e27ad7dbff772c108691
Thomas <thomascode92@hotmail.com>
2025-07-02 18:35:44 +0200

    Getting Started with the Web App

---
+++ concepts/modules/app-json/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+type RequestPayload struct {
+	Action  string `json:"action"`
+	Message string `json:"message"`
+}
+
+type ResponsePayload struct {
+	StatusCode int    `json:"status_code,omitempty"`
+	Message    string `json:"message"`
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/receive-post", receivePost)
+	mux.HandleFunc("/remote-server", remoteServer)
+
+	log.Println("Starting service...")
+
+	err := http.ListenAndServe(":8081", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func receivePost(w http.ResponseWriter, r *http.Request) {
+}
+
+func remoteServer(w http.ResponseWriter, r *http.Request) {
+}
