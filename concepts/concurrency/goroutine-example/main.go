3f2a54d0b3a4636a00a631a824f076f35fad1188
Thomas <thomascode92@hotmail.com>
2025-07-29 19:26:55 +0200

    WaitGroups to the Rescue

--- concepts/concurrency/goroutine-example/main.go
+++ concepts/concurrency/goroutine-example/main.go
@@ -2,17 +2,27 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-func printSomething(s string) {
+func printSomething(s string, wg *sync.WaitGroup) {
+	defer wg.Done() // Decrement the counter when the goroutine completes
 	fmt.Println(s)
 }
 
 func main() {
-	go printSomething("This is the first thing to be printed.")
+	var wg sync.WaitGroup
 
-	time.Sleep(1 * time.Second) // Wait for the goroutine to finish
+	words := []string{"alpha", "beta", "gamma", "delta", "phi", "zeta", "eta", "theta", "epsilon"}
+	wg.Add(len(words)) // Set the number of goroutines to wait for
 
-	printSomething("This is the second thing to be printed.")
+	for i, x := range words {
+		s := fmt.Sprintf("%d: %s", i+1, x)
+		go printSomething(s, &wg) // Start a goroutine for each word
+	}
+
+	wg.Wait() // wait for all goroutines to finish
+	wg.Add(1) // Add one more for the second print
+
+	printSomething("This is the second thing to be printed.", &wg)
 }
