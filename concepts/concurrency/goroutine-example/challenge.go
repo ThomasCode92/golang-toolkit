0eb14bac6e402e845ec7c0a38039d8e8b6cdcc82
Thomas <thomascode92@hotmail.com>
2025-07-30 08:21:21 +0200

    Challenge - Solution

--- concepts/concurrency/goroutine-example/challenge.go
+++ concepts/concurrency/goroutine-example/challenge.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
-var msg string
+var (
+	msg string
+	wg  sync.WaitGroup
+)
 
 func updateMessage(s string) {
+	defer wg.Done()
 	msg = s
 }
 
@@ -15,21 +20,26 @@ func printMessage() {
 }
 
 func challenge() {
-	// challenge: modify this code so that the calls to updateMessage() on lines
-	// 28, 30, and 33 run as goroutines, and implement wait groups so that
-	// the program runs properly, and prints out three different messages.
+	// challenge: modify this code so that the calls to updateMessage() run as
+	// goroutines and implement wait groups so that the program runs properly,
+	// and prints out three different messages.
 	// Then, write a test for all three functions in this program: updateMessage(),
 	// printMessage(), and main().
 
 	msg = "Hello, world!"
 
-	updateMessage("Hello, universe!")
+	wg.Add(1)
+	go updateMessage("Hello, universe!")
+	wg.Wait()
 	printMessage()
 
-	updateMessage("Hello, cosmos!")
+	wg.Add(1)
+	go updateMessage("Hello, cosmos!")
+	wg.Wait()
 	printMessage()
 
-	updateMessage("Hello, world!")
-
+	wg.Add(1)
+	go updateMessage("Hello, world!")
+	wg.Wait()
 	printMessage()
 }
