e4b483ebb42389f1d9e5704f75f437a045ed1fb9
Thomas <thomascode92@hotmail.com>
2025-08-19 20:46:33 +0200

    Getting Started

---
+++ projects/subscription-service/cmd/web/main.go
@@ -0,0 +1,19 @@
+package main
+
+const webPort = "8080"
+
+func main() {
+	// connect to the database
+
+	// create sessions
+
+	// create channels
+
+	// create waitGroup
+
+	// set up the application configuration
+
+	// set up mail
+
+	// listen for web connections
+}
