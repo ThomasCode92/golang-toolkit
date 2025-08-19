ad787f539216ce462bb4d01b60145432b04923c6
Thomas <thomascode92@hotmail.com>
2025-08-19 19:30:46 +0200

    Adding a Barber

--- concepts/concurrency/sleeping-barber/barbershop.go
+++ concepts/concurrency/sleeping-barber/barbershop.go
@@ -1,6 +1,10 @@
 package main
 
-import "time"
+import (
+	"time"
+
+	"github.com/fatih/color"
+)
 
 type BarberShop struct {
 	ShopCapacity    int
@@ -9,4 +13,47 @@ type BarberShop struct {
 	BarbersDoneChan chan bool
 	ClientsChan     chan string
 	Open            bool
+}
+
+func (b *BarberShop) addBarber(barber string) {
+	b.NumberOfBarbers++
+
+	go func() {
+		isSleeping := false
+		color.Yellow("%s goes to the waiting room to check for clients", barber)
+
+		for {
+			// if there are no clients, the barber goes to sleep
+			if len(b.ClientsChan) == 0 {
+				color.Yellow("there is nothing to do, so %s takes a nap", barber)
+				isSleeping = true
+			}
+
+			client, shopOpen := <-b.ClientsChan
+
+			if shopOpen {
+				if isSleeping {
+					color.Yellow("%s wakes %s up", client, barber)
+					isSleeping = false
+				}
+				// cut hair
+				b.cutHair(barber, client)
+			} else {
+				// shop is closed, so send the barber home (and close its goroutine)
+				b.sendBarberHome(barber)
+				return
+			}
+		}
+	}()
+}
+
+func (b *BarberShop) cutHair(barber, client string) {
+	color.Green("%s is cutting %s's hair", barber, client)
+	time.Sleep(b.HairCutDuration)
+	color.Green("%s finished cutting %s's hair", barber, client)
+}
+
+func (b *BarberShop) sendBarberHome(barber string) {
+	color.Cyan("%s is going home", barber)
+	b.BarbersDoneChan <- true
 }
