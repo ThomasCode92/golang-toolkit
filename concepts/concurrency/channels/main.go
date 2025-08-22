56bb5d948093db7848b14d6ef1c786e9d7111c6f
Thomas <108367944+ThomasCode92@users.noreply.github.com>
2025-08-22 16:57:04 +0200

    Channels, and Another Classic Problem (#10)

---
+++ concepts/concurrency/channels/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// shout has two parameters: a receive only chan ping, and a send only chan pong.
+// Note the use of <- in function signature. It simply takes whatever
+// string it gets from the ping channel,  converts it to uppercase and
+// appends a few exclamation marks, and then sends the transformed text to the pong channel.
+func shout(ping <-chan string, pong chan<- string) {
+	for {
+		// read from the ping channel. Note that the GoRoutine waits here -- it blocks until
+		// something is received on this channel.
+		s := <-ping
+		pong <- fmt.Sprintf("%s!!!", strings.ToUpper(s))
+	}
+}
+
+func main() {
+	// create two channels. Ping is what we send to, and pong is what comes back.
+	ping := make(chan string)
+	pong := make(chan string)
+
+	// start a goroutine
+	go shout(ping, pong)
+
+	fmt.Println("Type something and press ENTER (enter Q to quit).")
+	fmt.Println("Run 'select' example by typing 's' and pressing ENTER.")
+	fmt.Println("Run 'buffered' example by typing 'b' and pressing ENTER.")
+
+	for {
+		fmt.Print("-> ")
+
+		// get user input
+		var input string
+		_, _ = fmt.Scanln(&input)
+
+		if input == strings.ToLower("q") {
+			break // jump out of the loop
+		}
+
+		// run the example project to demonstrate the use of the select statement
+		if input == strings.ToLower("s") {
+			servers()
+			break // jump out of the loop
+		}
+
+		if input == strings.ToLower("b") {
+			buffers()
+			break // jump out of the loop
+		}
+
+		// send userInput to "ping" channel
+		ping <- input
+
+		// wait for a response from the pong channel. Again, program
+		// blocks (pauses) until it receives something from
+		// that channel.
+		response := <-pong
+
+		fmt.Println("response:", response)
+	}
+
+	fmt.Println("All done! Closing channels...")
+
+	// close the channels
+	close(ping)
+	close(pong)
+}
---
+++ concepts/concurrency/sleeping-barber/barbershop.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"time"
+
+	"github.com/fatih/color"
+)
+
+type BarberShop struct {
+	ShopCapacity    int
+	HairCutDuration time.Duration
+	NumberOfBarbers int
+	BarbersDoneChan chan bool
+	ClientsChan     chan string
+	Open            bool
+}
+
+func (b *BarberShop) addBarber(barber string) {
+	b.NumberOfBarbers++
+
+	go func() {
+		isSleeping := false
+		color.Yellow("%s goes to the waiting room to check for clients", barber)
+
+		for {
+			// if there are no clients, the barber goes to sleep
+			if len(b.ClientsChan) == 0 {
+				color.Yellow("there is nothing to do, so %s takes a nap", barber)
+				isSleeping = true
+			}
+
+			client, shopOpen := <-b.ClientsChan
+
+			if shopOpen {
+				if isSleeping {
+					color.Yellow("%s wakes %s up", client, barber)
+					isSleeping = false
+				}
+				// cut hair
+				b.cutHair(barber, client)
+			} else {
+				// shop is closed, so send the barber home (and close its goroutine)
+				b.sendBarberHome(barber)
+				return
+			}
+		}
+	}()
+}
+
+func (b *BarberShop) cutHair(barber, client string) {
+	color.Green("%s is cutting %s's hair", barber, client)
+	time.Sleep(b.HairCutDuration)
+	color.Green("%s finished cutting %s's hair", barber, client)
+}
+
+func (b *BarberShop) sendBarberHome(barber string) {
+	color.Cyan("%s is going home", barber)
+	b.BarbersDoneChan <- true
+}
+
+func (b *BarberShop) closeShopForDay() {
+	color.Cyan("Closing shop for the day.")
+
+	close(b.ClientsChan)
+	b.Open = false
+
+	for range b.NumberOfBarbers {
+		<-b.BarbersDoneChan
+	}
+
+	close(b.BarbersDoneChan)
+
+	color.Green("---------------------------------------------------------------------")
+	color.Green("The barbershop is now closed for the day, and everyone has gone home.")
+}
+
+func (b *BarberShop) addClient(client string) {
+	color.Green("*** %s arrives!", client)
+
+	if b.Open {
+		select {
+		case b.ClientsChan <- client:
+			color.Blue("%s takes a seat in the waiting room.", client)
+		default:
+			color.Red("The waiting room is full, %s leaves.", client)
+		}
+	} else {
+		color.Red("The shop is already closed, so %s leaves!", client)
+	}
+}
