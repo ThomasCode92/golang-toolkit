a2315e66b2d17f68aa63200142ef6d9071535a90
Thomas <thomascode92@hotmail.com>
2025-11-07 22:06:38 +0100

    Getting Started Testing Handlers

---
+++ projects/subscription-service/cmd/web/handlers_test.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"subscription-service/data"
+)
+
+var pageTests = []struct {
+	name               string
+	url                string
+	method             string
+	expectedStatusCode int
+	handler            http.HandlerFunc
+	sessionData        map[string]any
+	expectedHTML       string
+}{
+	{
+		name:               "home",
+		url:                "/",
+		method:             "GET",
+		expectedStatusCode: http.StatusOK,
+		handler:            testApp.HomePage,
+	},
+	{
+		name:               "login",
+		url:                "/login",
+		method:             "GET",
+		expectedStatusCode: http.StatusOK,
+		handler:            testApp.LoginPage,
+		expectedHTML:       `<h1 class="mt-5">Login</h1>`,
+	},
+	{
+		name:               "logout",
+		url:                "/logout",
+		method:             "GET",
+		expectedStatusCode: http.StatusSeeOther,
+		handler:            testApp.Logout,
+		sessionData: map[string]any{
+			"userID": 1,
+			"user":   data.User{},
+		},
+	},
+}
+
+func TestConfig(t *testing.T) {
+	pathToTemplates = "./templates/"
+
+	for _, e := range pageTests {
+		rr := httptest.NewRecorder()
+		req := httptest.NewRequest(e.method, e.url, nil)
+
+		ctx := getCtx(req)
+		req = req.WithContext(ctx)
+
+		if len(e.sessionData) > 0 {
+			for key, value := range e.sessionData {
+				testApp.Session.Put(ctx, key, value)
+			}
+		}
+
+		e.handler.ServeHTTP(rr, req)
+
+		if rr.Code != e.expectedStatusCode {
+			t.Errorf("%s failed: expected %d, got %d", e.name, e.expectedStatusCode, rr.Code)
+		}
+
+		if len(e.expectedHTML) > 0 {
+			html := rr.Body.String()
+			if !strings.Contains(html, e.expectedHTML) {
+				t.Errorf("%s failed: expected HTML to contain %s", e.name, e.expectedHTML)
+			}
+		}
+	}
+}
