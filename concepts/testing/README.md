86c35d54a52ccedb9dc90fb87fa9f67ce481829e
Thomas <108367944+ThomasCode92@users.noreply.github.com>
2025-11-15 15:03:00 +0100

    Simple Testing Concepts (#16)

---
+++ concepts/testing/README.md
@@ -0,0 +1,72 @@
+# Introduction to Testing in Go
+
+Learn how to write effective unit and integration tests in Go, for web
+applications and REST APIs
+
+---
+
+## 📚 Testing in Go
+
+- Testing is a critical part of the development process.
+- Well tested code is more maintainable, more secure, and overall takes less
+  time to write.
+- Go has a rich set of tools for testing built right integration.
+- We wont't use any third party testing packages at all (e.g. Testify).
+- We'll write unit tests and integration tests.
+
+## ❓ About this project
+
+> This project is based on the
+> [Introduction to Testing in Go](https://www.udemy.com/course/introduction-to-testing-in-go-golang/)
+> course of Travis Sawler.
+
+### 🧠 Topics & Features
+
+- Prime number CLI
+  - A simple command line program that takes user input.
+  - The program will determine whether or not a given number is prime.
+  - We'll write tests for that program.
+- Simple Web Applications
+  - We'll build a simple web application that allows users to authenticate
+    (securely).
+  - We'll write some middleware to check authentication, and to add some data to
+    the context.
+  - We'll also use the _Repository Pattern_ to make the code more testable.
+  - Write tests for all functionality, including integration tests.
+- REST API
+  - We'll write a simple REST API that allows authentication using JWT tokens.
+  - We'll write some middleware to check authentication by looking at the
+    Authorization header for incoming requests.
+  - We'll create endpoints for typical CRUD operations.
+  - Write tests for all functionality.
+- Update REST API to support SPAs
+  - We'll update our REST API to include functionality that makes it useable
+    with any Single Page Application (SPA).
+  - We'll add functionality to store JWTs and Refresh tokens securely.
+  - We'll create endpoints for typical SPA functionality.
+  - Write tests for all functionality.
+
+### 📁 Structure
+
+```
+testing/
+├── primeapp/     # simple prime number CLI application
+└──
+```
+
+### 🚀 Running the Example Applications
+
+Each subfolder is a Go project. To run the example applications, you can use the
+following commands:
+
+```bash
+## running the application
+docker compose up -d      # start any required services (e.g. databases)
+go run ./...              # run the application
+
+## test related commands
+go test ./...                             # run all tests
+go test -cover ./...                      # run all tests with coverage report
+go test -coverprofile=coverage.out ./...  # generate coverage profile
+go tool cover -html=coverage.out          # view coverage report in browser
+```
---
+++ concepts/testing/primeapp/main_test.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"io"
+	"os"
+	"strings"
+	"testing"
+)
+
+func Test_intro(t *testing.T) {
+	oldOut := os.Stdout  // save a copy of os.Stdout
+	r, w, _ := os.Pipe() // create a read and write pipe
+	os.Stdout = w        // set os.Stdout to the write pipe
+
+	intro() // call the function that prints to stdout
+
+	_ = w.Close()           // close the write pipe
+	os.Stdout = oldOut      // reset os.Stdout to its original value
+	out, _ := io.ReadAll(r) // read the output from the read pipe
+
+	// perform the test
+	if !strings.Contains(string(out), "Enter a whole number") {
+		t.Errorf("intro text not correct, got %s", string(out))
+	}
+}
+
+func Test_prompt(t *testing.T) {
+	oldOut := os.Stdout  // save a copy of os.Stdout
+	r, w, _ := os.Pipe() // create a read and write pipe
+	os.Stdout = w        // set os.Stdout to the write pipe
+
+	prompt() // call the function that prints to stdout
+
+	_ = w.Close()           // close the write pipe
+	os.Stdout = oldOut      // reset os.Stdout to its original value
+	out, _ := io.ReadAll(r) // read the output from the read pipe
+
+	// perform the test
+	if string(out) != "-> " {
+		t.Errorf("incorrect prompt: expected '->', but got %s", string(out))
+	}
+}
+
+func Test_readUserInput(t *testing.T) {
+	// to test this function, we need a channel, and an instance of an io.Reader
+	doneChan := make(chan bool)
+
+	// create a reference to a bytes.Buffer
+	var stdin bytes.Buffer
+
+	stdin.Write([]byte("1\nq\n"))
+
+	go readUserInput(&stdin, doneChan)
+	<-doneChan
+	close(doneChan)
+}
+
+func Test_checkNumbers(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{name: "empty", input: "", expected: "Please enter a whole number!"},
+		{name: "zero", input: "0", expected: "0 is not prime, by definition!"},
+		{name: "one", input: "1", expected: "1 is not prime, by definition!"},
+		{name: "two", input: "2", expected: "2 is a prime number!"},
+		{name: "three", input: "3", expected: "3 is a prime number!"},
+		{name: "negative", input: "-1", expected: "Negative numbers are not prime, by definition!"},
+		{name: "typed", input: "three", expected: "Please enter a whole number!"},
+		{name: "decimal", input: "1.1", expected: "Please enter a whole number!"},
+		{name: "quit", input: "q", expected: ""},
+		{name: "QUIT", input: "Q", expected: ""},
+		{name: "greek", input: "επτά", expected: "Please enter a whole number!"},
+	}
+
+	for _, e := range tests {
+		input := strings.NewReader(e.input)
+		reader := bufio.NewScanner(input)
+		res, _ := checkNumbers(reader)
+
+		if !strings.EqualFold(res, e.expected) {
+			t.Errorf("%s: expected %s, but got %s", e.name, e.expected, res)
+		}
+	}
+}
+
+func Test_isPrime(t *testing.T) {
+	primeTests := []struct {
+		name     string
+		testNum  int
+		expected bool
+		msg      string
+	}{
+		{"prime", 7, true, "7 is a prime number!"},
+		{"not prime", 8, false, "8 is not a prime number because it is divisible by 2!"},
+		{"zero", 0, false, "0 is not prime, by definition!"},
+		{"one", 1, false, "1 is not prime, by definition!"},
+		{"negative number", -11, false, "Negative numbers are not prime, by definition!"},
+	}
+
+	for _, e := range primeTests {
+		result, msg := isPrime(e.testNum)
+		if e.expected && !result {
+			t.Errorf("%s: expected true but got false", e.name)
+		}
+
+		if !e.expected && result {
+			t.Errorf("%s: expected false but got true", e.name)
+		}
+
+		if e.msg != msg {
+			t.Errorf("%s: expected %s but got %s", e.name, e.msg, msg)
+		}
+	}
+}
