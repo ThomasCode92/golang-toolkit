11e326694920e351bde590cf4095a08f9ca23d40
Thomas <thomascode92@hotmail.com>
2025-11-19 21:21:08 +0100

    Testing the Application Routes

---
+++ concepts/testing/webapp/cmd/web/routes_test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func Test_application_Home(t *testing.T) {
+	registered := []struct {
+		route  string
+		method string
+	}{
+		{route: "/", method: "GET"},
+		{route: "/static/*", method: "GET"},
+	}
+
+	var app application
+	mux := app.routes()
+	chiRouter := mux.(chi.Routes) // cast to chi.Routes to access the routes
+
+	for _, route := range registered {
+		// check to see if the route exists
+		if !routeExists(route.route, route.method, chiRouter) {
+			t.Errorf("route %s is not registered", route.route)
+		}
+	}
+}
+
+func routeExists(testRoute, testMethod string, chiRoutes chi.Routes) bool {
+	found := false
+
+	_ = chi.Walk(chiRoutes, func(method string, route string, handler http.Handler, middlewares ...func(http.Handler) http.Handler) error {
+		if strings.EqualFold(method, testMethod) && strings.EqualFold(route, testRoute) {
+			found = true
+		}
+		return nil
+	})
+
+	return found
+}
