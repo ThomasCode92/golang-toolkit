5d24c832aa3ad9f119ce3a6af83f6513702a0deb
Thomas <thomascode92@hotmail.com>
2026-01-05 22:39:44 +0100

    Adding Content to the Body Component

--- projects/books-manager/src/App.vue
+++ projects/books-manager/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import AppHeader from './components/AppHeader.vue'
+import AppBody from './components/AppBody.vue'
 </script>
 
 <template>
   <AppHeader />
+  <AppBody />
 </template>
 
 <style scoped></style>
