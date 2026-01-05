48fe4b312193b7ba5d099ff76225f8f4da03dff5
Thomas <108367944+ThomasCode92@users.noreply.github.com>
2026-01-05 21:17:30 +0100

    Getting Started with Vue (#19)

---
+++ projects/book-manager/forms.js
@@ -0,0 +1,61 @@
+const TextInput = {
+  props: {
+    name: String,
+    type: String,
+    label: String,
+    placeholder: String,
+    required: String,
+    min: String,
+    max: String,
+    value: String,
+  },
+  template: `
+    <div class="mb-3">
+      <label :for="name" class="form-label">{{ label }}</label>
+      <input
+        :id="name"
+        :type="type"
+        :name="name"
+        class="form-control"
+        :placeholder="placeholder"
+        :autocomplete="name + '-new'"
+        :value="value"
+        :required="required"
+        :min="min"
+        :max="max"
+      />
+    </div>
+  `,
+};
+
+const SelectInput = {
+  props: ["items", "name", "label", "required"],
+  template: `
+    <div class="mb-3">
+      <label :for="name" class="form-label">{{label}}</label>
+      <select :id="name" class="form-select" :name="name" :required="required">
+        <option v-for="item in items" :key="item.id" :value="item.value">
+          {{ item.text }}
+        </option>
+      </select>
+    </div>
+  `,
+};
+
+const CheckboxInput = {
+  props: ["name", "label", "required", "value", "checked"],
+  template: `
+    <div class="form-check mb-3">
+      <input
+        :id="name"
+        type="checkbox"
+        :name="name"
+        class="form-check-input"
+        :value="value"
+        :checked="checked"
+        :required="required"
+      />
+      <label :for="name" class="form-check-label">{{ label }}</label>
+    </div>
+  `,
+};
