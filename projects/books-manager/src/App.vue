2cc59595d0b9b6bbda467a0d54b913fd88c33fa1
Thomas <thomascode92@hotmail.com>
2026-01-05 22:53:16 +0100

    Adding Content to the Footer Component

--- projects/books-manager/src/App.vue
+++ projects/books-manager/src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-import AppHeader from './components/AppHeader.vue'
 import AppBody from './components/AppBody.vue'
+import AppFooter from './components/AppFooter.vue'
+import AppHeader from './components/AppHeader.vue'
 </script>
 
 <template>
   <AppHeader />
   <AppBody />
+  <AppFooter />
 </template>
 
 <style scoped></style>
---
+++ projects/books-manager/src/components/AppFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="d-flex align-items-center justify-content-center mt-5">
+    Copyright (c) {{ new Date().getFullYear() }} Thomas. All Rights Reserved.
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  background-color: var(--bs-body-color);
+  color: white;
+  min-height: 3rem;
+  font-size: 0.8rem;
+  width: 100%;
+  position: fixed;
+  bottom: 0;
+}
+</style>
