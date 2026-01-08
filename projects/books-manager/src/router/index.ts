c2a60167d567a4a42bedc2ca93beb741a93cbac9
Thomas <thomascode92@hotmail.com>
2026-01-08 20:57:15 +0100

    Creating a Login Form Component and Update Navigation Links

--- projects/books-manager/src/router/index.ts
+++ projects/books-manager/src/router/index.ts
@@ -1,8 +1,12 @@
 import { createRouter, createWebHistory, type RouterOptions } from 'vue-router'
 
 import AppBody from '@/components/AppBody.vue'
+import LoginForm from '@/components/LoginForm.vue'
 
-const routes: RouterOptions['routes'] = [{ path: '/', name: 'home', component: AppBody }]
+const routes: RouterOptions['routes'] = [
+  { path: '/', name: 'home', component: AppBody },
+  { path: '/login', name: 'login', component: LoginForm },
+]
 
 export const router = createRouter({
   history: createWebHistory(),
