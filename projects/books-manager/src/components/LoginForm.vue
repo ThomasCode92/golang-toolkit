c11857f5796c0ceb9bf8f34597e18431741309de
Thomas <thomascode92@hotmail.com>
2026-01-10 21:19:47 +0100

    Adding some Form Input Components

--- projects/books-manager/src/components/LoginForm.vue
+++ projects/books-manager/src/components/LoginForm.vue
@@ -1,8 +1,38 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+
+import TextInput from './forms/TextInput.vue'
+import { validateForms } from '@/utils/validation'
+
+onMounted(() => {
+  validateForms()
+})
+</script>
+
 <template>
   <div class="container">
     <div class="row">
       <div class="col">
         <h1 class="mt-5">Login</h1>
+        <hr />
+        <form method="POST" action="/login" class="needs-validation" novalidate>
+          <text-input
+            label="Email"
+            type="email"
+            name="email"
+            placeholder="Enter your email"
+            required
+          />
+          <text-input
+            label="Password"
+            type="password"
+            name="password"
+            placeholder="Enter your password"
+            required
+          />
+          <hr />
+          <input type="submit" class="btn btn-primary" value="Login" />
+        </form>
       </div>
     </div>
   </div>
---
+++ projects/books-manager/src/components/forms/CheckboxInput.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps(['label', 'name', 'value', 'checked', 'required'])
+</script>
+
+<template>
+  <div class="form-check mb-3">
+    <input
+      :id="name"
+      type="checkbox"
+      :name="name"
+      class="form-check-input"
+      :value="value"
+      :checked="checked"
+      :required="required"
+    />
+    <label :for="name" class="form-check-label">{{ label }}</label>
+  </div>
+</template>
