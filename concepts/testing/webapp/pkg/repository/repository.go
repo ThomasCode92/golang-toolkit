66e70ed5a320d789b95fa041f52ff5032712203b
Thomas <thomascode92@hotmail.com>
2026-03-28 15:57:50 +0100

    Updating Application Config to Use the Database Repository

--- concepts/testing/webapp/pkg/repository/repository.go
+++ concepts/testing/webapp/pkg/repository/repository.go
@@ -11,8 +11,8 @@ type DatabaseRepo interface {
 	AllUsers() ([]*data.User, error)
 	GetUser(id int) (*data.User, error)
 	GetUserByEmail(email string) (*data.User, error)
-	InsertUser(user *data.User) (int, error)
-	UpdateUser(user *data.User) error
+	InsertUser(user data.User) (int, error)
+	UpdateUser(user data.User) error
 	DeleteUser(id int) error
 	ResetPassword(id int, password string) error
 	InsertUserImage(i data.UserImage) (int, error)
